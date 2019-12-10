afd8b4217e20a6c831c0bfc52cdfe3d0ed3cd93b
Thiago Augusto <martinsthiagoaugusto@gmail.com>
2019-12-09 21:28:07 -0300

    Filter null reads

--- functions/index.js
+++ functions/index.js
@@ -8,7 +8,8 @@ exports.rawToWav = functions.https.onRequest((req, res) => {
   const {rawAudio} = req.body; 
   const LIMITE = 32767;
   const RANGE_ANALOG = 1024;
-  audioInput = rawAudio.map(item => item * LIMITE/RANGE_ANALOG)
+  audioInput = rawAudio.filter(item => item != null);
+  audioInput = rawAudio.map(item => item * LIMITE/RANGE_ANALOG);
   wav.fromScratch(1, 2300, '16', audioInput);
   const buff = Buffer.from(wav.toBuffer())
   res.json(buff.toString('base64'))  
